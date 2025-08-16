6011598d169347aa95b314942f111fe8af28c090
TofSas22 <tsasman13@gmail.com>
2025-08-17 00:40:59 +0200

    Init project and made it possible to prompt with the uv run main.py command

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents=user_prompt
+    )
+
+    print(f"sys.argv: {sys.argv[1:]}")
+    print(f"Prompt: {user_prompt}")
+    print("\nResponse:")
+    print(response.text)
+    print(f"\nPrompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
